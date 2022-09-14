963bf19df88b1165673cc426d5c0308b8b2201e4
R13 <lazarvukadinovic7@gmail.com>
2022-09-14 08:28:42 +0200

    dopuna

--- rezervacijaBioskop.php
+++ rezervacijaBioskop.php
@@ -5,43 +5,110 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Bioskop Online</title>
+        <style>
+            .error {color: #FF0000;}
+        </style>
     </head>
     <body>
         <?php 
+            $name = $email = $film = $termin = $sedista = "";
+            $nameErr = $emailErr = $filmErr = $terminErr = $sedistaErr = "";
             $filmovi = array("Juzni Vetar Ubrzanje", "Tesna Koza", "Titanik");
+
+            $imeREG = "/([A-Za-z ])/";
+            $sedistaREG = "/^([0-9 ,])+$/";
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                if (empty($_POST["ime"])) {
+                  $nameErr = "Ime je obavezno";
+                } else {
+                    $nameErr = "";
+                    $name = test_input($_POST["ime"]);
+                    if(!preg_match($imeREG, $name))
+                    {
+                        $nameErr = "Neispravan format imena";
+                        $name = "";
+                    }
+                }
+                
+                if (empty($_POST["email"])) {
+                  $emailErr = "Email je obavezan";
+                } else {
+                    $emailErr = "";
+                  $email = test_input($_POST["email"]);
+                  if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+                  {
+                    $emailErr = "Neispravan format";
+                    $email = "";
+                  }
+                }
+                  
+                if (empty($_POST["izaberiFilm"])) {
+                  $filmErr = "Morate odabrati film";
+                } else {
+                    $filmErr = "";
+                  $film = test_input($_POST["izaberiFilm"]);
+                }
+              
+                if (empty($_POST["izaberiTermin"])) {
+                  $terminErr = "Morate odabrati termin";
+                } else {
+                    $terminErr = "";
+                  $termin = test_input($_POST["izaberiTermin"]);
+                }
+              
+                if (empty($_POST["sedista"])) {
+                  $sedistaErr = "Morate odabrati sedista";
+                } else {
+                    $sedistaErr = "";
+                  $sedista = test_input($_POST["sedista"]);
+                  if(!filter_var($sedista, $sedistaREG))
+                  {
+                    $sedistaErr = "Mozete uneti brojeve, zarez i razmak";
+                  }
+                }
+              }
+              
+              function test_input($data) {
+                $data = trim($data);
+                $data = stripslashes($data);
+                $data = htmlspecialchars($data);
+                return $data;
+              }
+
+
 
         ?>
         <h1 style="text-align: center;">Rezervacija bioskopske karte</h1>
 
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 
             <label for="ime">Ime: </label>
-            <input type="text" name="ime">
+            <input type="text" name="ime"> <span class="error">* <?php echo $nameErr;?></span>
             <br>
             <label for="email">Email: </label>
-            <input type="email" name="email">
+            <input type="text" name="email"> <span class="error">* <?php echo $emailErr;?></span>
             <br>
-            <select name="izaberiFilm" id="izaberiFilm">
+            <label for="izaberiFilm">Film</label>
+            <select name="izaberiFilm" id="izaberiFilm"> 
                 <?php 
                     for($i=0; $i<count($filmovi); $i++)
                     echo '<option value="film' . $i . '">' . $filmovi[$i] .'</option>';
                 ?>
                 
-            </select>
-            <select name="izaberiTermin" id="izaberiTermin">
+            </select> <span class="error">* <?php echo $filmErr;?></span>
+            <br>
+            <label for="izaberiTermin">Termin</label>
+            <select name="izaberiTermin" id="izaberiTermin"> 
                 <option value="vreme0">16h</option>
                 <option value="vreme1">18h</option>
                 <option value="vreme2">20h</option>
-            </select>
-            <select name="izaberiMesto" id="izaberiMesto">
-                <?php 
-                    $sedista = array(rand(1,70), rand(1,70), rand(1,70), rand(1,70), rand(1,70));
-                    for($i=0; $i<count($sedista); $i++)
-                    {
-                        echo '<option value="film' . $i . '">' . $sedista[$i] .'</option>';
-                    }
-                ?>
-            </select>
+            </select><span class="error">* <?php echo $terminErr;?></span>
+            <br>
+            <label for="sedista">Broj sedista</label> 
+            <input type="text" name="sedista"> <span class="error">* <?php echo $sedistaErr;?></span>
+            <br>
+            <input type="submit" value="Rezervisi">
         </form>
     </body>
 </html>
