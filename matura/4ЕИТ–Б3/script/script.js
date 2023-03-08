c5e2567f4d732edf0daf94535e46f47240735f9f
Lazar <lazolakivukadinovic1001@gmail.com>
2023-03-08 18:27:14 +0100

    script update

--- matura/4ЕИТ–Б3/script/script.js
+++ matura/4ЕИТ–Б3/script/script.js
@@ -1,11 +1,21 @@
 const drzave = ['Srbija', 'Madjarska', 'Rumunija', 'Bugarska', 'Makedonija', 'Albanija', 'Crna Gora', 'Bosna', 'Hrvatska'];
 
-for(let i=0; i<=9; i++)
+for(let i=0; i<9; i++)
 {
-    console.log(i)
     document.getElementById('p'+(i+1)).addEventListener('click', function(){
-        window.open('./pages/info/' + drzave[i] +'.html','name','width=300, height=200')
-    })
+        window.open('./pages/info/' + drzave[i] +'.html','name','width=500, height=500')
+    });
+}
+
+for(let i=0; i<9; i++)
+{
+    document.getElementById('img'+(i+1)).addEventListener('mouseover', function() {
+        PlaySound('audio'+(i+1));
+    });
+
+    document.getElementById('img'+(i+1)).addEventListener('mouseout', function() {
+        StopSound('audio'+(i+1));
+    });
 }
 
 function PlaySound(soundobj) {
