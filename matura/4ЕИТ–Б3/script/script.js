6b15032172e2116b71ae01fac489a89e916f7665
Lazar Vukadinovic <lazarvukadinovic7@gmail.com>
2023-03-08 09:27:15 +0100

    KRI

--- matura/4ЕИТ–Б3/script/script.js
+++ matura/4ЕИТ–Б3/script/script.js
@@ -1,8 +1,20 @@
 const drzave = ['Srbija', 'Madjarska', 'Rumunija', 'Bugarska', 'Makedonija', 'Albanija', 'Crna Gora', 'Bosna', 'Hrvatska'];
 
-for(let i=0; i<10; i++)
+for(let i=0; i<=9; i++)
 {
+    console.log(i)
     document.getElementById('p'+(i+1)).addEventListener('click', function(){
         window.open('./pages/info/' + drzave[i] +'.html','name','width=300, height=200')
     })
+}
+
+function PlaySound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.play();
+}
+
+function StopSound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.pause();
+    thissound.currentTime = 0;
 }
