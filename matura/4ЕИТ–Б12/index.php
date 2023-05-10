2e097b79e791de38845cc68e7662519b0f1a307d
Lazar Vukadinovic <lazarvukadinovic7@gmail.com>
2023-05-10 09:17:05 +0200

    b12 start

---
+++ matura/4ЕИТ–Б12/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Autobuska stanica</title>
+    <link rel="stylesheet" href="./styles/style.css">
+</head>
+
+<body>
+    <div class="nav">
+        <h1>Rezervacija autobuskih karata</h1>
+    </div>
+    <div class="navigacija">
+        <a href="./">Pocetna</a>
+        <a href="./pages/about.php">O autoru</a>
+        <a href="./pages/uputstvo.html">Uputstvo</a>
+    </div>
+
+    <div class="bus">
+        <table>
+            <?php 
+                include "./database/connection.php";
+
+                $j=2;
+                for($i=0; $i<13; $i++)
+                {
+            ?>
+                <tr>
+                    <th class="celija"><button class="" id="<?php echo $j; ?>"><?php echo $j++; ?></button></th>
+                    <th class="celija"><button class="" id="<?php echo $j; ?>"><?php echo $j++; ?></button></th>
+                    <th></th>
+                    <th class="celija"><button class="" id="<?php echo $j; ?>"><?php echo $j++; ?></button></th>
+                    <th class="celija"><button class="" id="<?php echo $j; ?>"><?php echo $j++; ?></button></th>
+                </tr>
+            <?php }?>
+        </table>
+    </div>
+
+    <div class="footer">
+        <p>© Autobuska stanica</p>
+    </div>
+
+</body>
+
+</html>
